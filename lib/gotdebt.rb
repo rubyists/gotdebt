e884ed939228416df2a5edb4f4d29c6648b445b5
Tj Vanderpoel <tj@rubyists.com>
2013-09-10 15:59:25 -0500

    got em comitted

---
+++ lib/gotdebt.rb
@@ -0,0 +1,19 @@
+require "pathname"
+#$LOAD_PATH.unshift(File.expand_path("../", __FILE__))
+
+# Allows for pathnames to be easily added to
+class Pathname
+  def /(other)
+    join(other.to_s)
+  end
+end
+
+# The Gotdebt library, by rciweb
+module Gotdebt
+  ROOT = Pathname(Pathname(File.dirname(__FILE__) + "/..").expand_path) unless Gotdebt.const_defined?("ROOT")
+  LIBDIR = ROOT/:lib unless Gotdebt.const_defined?("LIBDIR")
+  L = LIBDIR/:gotdebt unless Gotdebt.const_defined?("L")
+  M = ROOT/:model unless Gotdebt.const_defined?("M")
+  C = ROOT/:controller unless Gotdebt.const_defined?("C")
+end
+require (Gotdebt::LIBDIR/"version").to_s
---
+++ lib/version.rb
@@ -0,0 +1,3 @@
+module Gotdebt
+  VERSION = '0.0.1'
+end
